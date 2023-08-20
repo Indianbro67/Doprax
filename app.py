7284cf4cfff3e633b565f40ce1546d2f73ede785
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-08-20 11:02:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+import requests
+import schedule
+import time
 
-@app.route('/')
-def hello_world():
-    return 'Welcome To Nia Chatbot basic'
+def delete_webhook():
+    bot_token = "5835400119:AAG05VIHGiaNvefr3DKF4yd8bTAcZLSyxx4"
+    api_url = f"https://api.telegram.org/bot{bot_token}/deleteWebhook"
 
+    response = requests.post(api_url)
 
-if __name__ == "__main__":
-    app.run()
+    if response.status_code == 200:
+        print("Webhook deleted successfully.")
+    else:
+        print(f"Failed to delete webhook. Status code: {response.status_code}")
+
+schedule.every(12).hours.do(delete_webhook)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
