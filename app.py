0b2f68d594b02f4eda1f16425d07a455aaf4573f
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-08-22 11:31:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,60 @@
+import random
+import os
 import requests
-import schedule
-import time
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-def delete_webhook():
-    bot_token = "6416227615:AAG6WpsPt0GgZzzlxIFOj2m9T0aBZ0NTb0I"
-    api_url = f"https://api.telegram.org/bot{bot_token}/deleteWebhook"
+# Replace with your actual bot token
+TOKEN = '6028447687:AAGJtVpsUDL6KAzDzcm6LPT5nYN9rjQwTuQ'
 
-    response = requests.post(api_url)
+# List of allowed video extensions
+allowed_extensions = ['.mp4', '.mov']
 
-    if response.status_code == 200:
-        print("Webhook deleted successfully.")
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Send /reel to get a random video!")
+
+def reel(update: Update, context: CallbackContext) -> None:
+    # List of video URLs
+    video_urls = [
+        "https://cdn.discordapp.com/attachments/1101163334692245546/1101164269845889.mp4",
+        "https://cdn.discordapp.com/attachments/1101163334692245546/1101164241156841.mp4",
+        "https://cdn.discordapp.com/attachments/1101163334692245546/1101164052413165.mp4",
+        "https://cdn.discordapp.com/attachments/1101163334692245546/1101164246101925.mp4",
+    ]
+
+    # Filter video URLs by allowed extensions
+    valid_video_urls = [url for url in video_urls if any(url.endswith(ext) for ext in allowed_extensions)]
+
+    if valid_video_urls:
+        random_video_url = random.choice(valid_video_urls)
+
+        # Download video
+        response = requests.get(random_video_url)
+        if response.status_code == 200:
+            with open("downloaded_video.mp4", "wb") as f:
+                f.write(response.content)
+
+            # Send video to bot
+            with open("downloaded_video.mp4", "rb") as video_file:
+                update.message.reply_video(video_file)
+
+            # Delete downloaded video
+            os.remove("downloaded_video.mp4")
+
+        else:
+            update.message.reply_text("Failed to download video.")
     else:
-        print(f"Failed to delete webhook. Status code: {response.status_code}")
+        update.message.reply_text("No valid videos available.")
 
-schedule.every(1).second.do(delete_webhook)
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dp = updater.dispatcher
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("reel", reel))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
